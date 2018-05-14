69d9b65f6ed2dcd531c7a139e0649fa5fe6df479
Stefan Partusch <s.t.partusch@gmail.com>
2018-05-14 16:26:26 +0200

    Add ability to filter user agents in request logging

--- src/main/java/com/github/spartusch/rateretriever/rate/v1/controller/RequestLoggingFilter.java
+++ src/main/java/com/github/spartusch/rateretriever/rate/v1/controller/RequestLoggingFilter.java
@@ -2,21 +2,36 @@ package com.github.spartusch.rateretriever.rate.v1.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 @Component
 public class RequestLoggingFilter implements WebFilter {
 
     private static final Logger log = LoggerFactory.getLogger(RequestLoggingFilter.class);
 
+    private final String suppressedUserAgent;
+
+    public RequestLoggingFilter(@Value("${requests.logging.suppressUserAgent}") final String suppressedUserAgent) {
+        Objects.requireNonNull(suppressedUserAgent);
+        log.info("Suppressing logs of requests by user agent: {}", suppressedUserAgent);
+        this.suppressedUserAgent = suppressedUserAgent;
+    }
+
     @Override
     public Mono<Void> filter(final ServerWebExchange exchange, final WebFilterChain chain) {
-        log.info("Request: {}", exchange.getRequest().getURI());
-        log.debug("Headers: {}", exchange.getRequest().getHeaders());
+        final var headers = exchange.getRequest().getHeaders();
+        final var userAgent = headers.getFirst("User-Agent");
+        if (userAgent.isEmpty() || !userAgent.startsWith(suppressedUserAgent)) {
+            log.info("Request: {}", exchange.getRequest().getURI());
+            log.debug("Headers: {}", headers);
+        }
         return chain.filter(exchange);
     }
 }
