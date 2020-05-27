d13f77c04b6bf34c2d8a28a7e9ca7997bd283df9
Stefan Partusch <s.t.partusch+gh@gmail.com>
2020-05-27 09:14:53 +0200

    Update admin URL

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-declare $(env -i `cat bin/configuration.vars`)
-
 ./gradlew -PgenerateLaunchScript bootJar
-./build/libs/simple-rate-retriever*.jar --spring.boot.admin.client.url=$admin_server
+./build/libs/simple-rate-retriever*.jar --spring.boot.admin.client.url=http://localhost:18000
