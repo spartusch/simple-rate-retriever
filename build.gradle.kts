121a7abe437337f43169e3263df9a13b81466ab0
Stefan Partusch <s.t.partusch+gh@gmail.com>
2020-12-03 22:43:25 +0100

    Upgrade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
-    val kotlinVersion = "1.4.0"
+    val kotlinVersion = "1.4.20"
 
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("org.springframework.boot") version "2.3.3.RELEASE"
+    id("org.springframework.boot") version "2.3.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("com.gorylenko.gradle-git-properties") version "2.2.3"
-    id("io.gitlab.arturbosch.detekt") version "1.12.0"
+    id("com.gorylenko.gradle-git-properties") version "2.2.4"
+    id("io.gitlab.arturbosch.detekt") version "1.14.2"
 }
 
 val jvmTarget = "11"
@@ -33,20 +33,20 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.+")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:latest.release")
 
-    implementation("com.github.spartusch:excel-web-query:2.+")
+    implementation("com.github.spartusch:excel-web-query:+")
 
     // Monitoring
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("de.codecentric:spring-boot-admin-starter-client:2.+")
+    implementation("de.codecentric:spring-boot-admin-starter-client:latest.release")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.github.tomakehurst:wiremock-standalone:2.+")
-    testImplementation("com.github.JensPiegsa:wiremock-extension:0.4.+")
+    testImplementation("com.github.tomakehurst:wiremock-standalone:latest.release")
+    testImplementation("com.github.JensPiegsa:wiremock-extension:latest.release")
 
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.+")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:latest.release")
 }
 
 springBoot {
