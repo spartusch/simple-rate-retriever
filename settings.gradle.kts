5178a3a3830f2f4e38bc8002205700351f2ede49
Stefan Partusch <s.t.partusch+gh@gmail.com>
2022-02-14 18:05:09 +0100

    Update versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,10 @@ pluginManagement {
         kotlin("plugin.spring") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
 
-        id("org.springframework.boot") version "2.6.2"
+        id("org.springframework.boot") version "2.6.3"
         id("io.spring.dependency-management") version "1.0.11.RELEASE"
-        id("org.openapi.generator") version "5.3.1"
-        id("com.gorylenko.gradle-git-properties") version "2.3.2"
+        id("org.openapi.generator") version "5.4.0"
+        id("com.gorylenko.gradle-git-properties") version "2.4.0"
         id("io.gitlab.arturbosch.detekt") version "1.19.0"
     }
 }
