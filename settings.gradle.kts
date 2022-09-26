c8ca3b1c247f63c6c351542568a4763c5db0fd98
Stefan Partusch <s.t.partusch+gh@gmail.com>
2022-09-26 19:13:06 +0200

    Update dependencies and upgrade Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
         kotlin("plugin.spring") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
 
-        id("org.springframework.boot") version "2.7.1"
-        id("io.spring.dependency-management") version "1.0.11.RELEASE"
+        id("org.springframework.boot") version "2.7.4"
+        id("io.spring.dependency-management") version "1.0.14.RELEASE"
         id("org.openapi.generator") version "6.0.1"
         id("com.gorylenko.gradle-git-properties") version "2.4.1"
         id("io.gitlab.arturbosch.detekt") version "1.21.0"
